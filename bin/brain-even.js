abb57404d6962d43c125fba4ee47595c372ee3f1
root <root@DESKTOP-760JIBS>
2023-05-27 23:09:39 +0300

    Fix brain-even description

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -5,7 +5,7 @@ import readlineSync from 'readline-sync';
 console.log('Welcome to the Brain Games!');
 const name = readlineSync.question('May I have your name? ');
 console.log(`Hello, ${name}!`);
-console.log('Answer "yes" if number is odd otherwise answer "no".');
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
 let correctAnswers = 0;
 
