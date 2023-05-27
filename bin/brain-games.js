0abd738a537f237faef9b5141325e4998651ea21
root <root@DESKTOP-760JIBS>
2023-05-27 18:19:16 +0300

    Исправлен JSON файл

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
-import greeting from '/mnt/c/Users/XEON-12/Desktop/frontend-project-44-main/src/cli';
+import greeting from '/project/src/cli';
 
 console.log('Welcome to the Brain Games!');
 const name = readlineSync.question('May I have your name? ');
