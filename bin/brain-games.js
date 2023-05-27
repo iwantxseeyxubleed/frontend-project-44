9dc0cec8ff085cdbb5350bbee5bb4b74aed27063
root <root@DESKTOP-760JIBS>
2023-05-27 22:14:47 +0300

    upd

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
-import greeting from '../src/cli.cjs';
+import greeting from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!');
 const name = readlineSync.question('May I have your name? ');
