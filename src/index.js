66203a2efb6b8dc53fec4540c15a877892e524b2
root <root@DESKTOP-760JIBS>
2023-05-28 23:30:12 +0300

    Update

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import readlineSync from 'readline-sync';
 
 global.questionsNumber = 3;
-const maxRandomNumber = 100; // less than it and not equal
+const maxRandomNumber = 100;
 
 export function getUserInput(question) {
   return readlineSync.question(question);
