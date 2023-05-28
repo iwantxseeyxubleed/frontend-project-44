29e2f13c2caae9083da45b78f0855243fef65470
root <root@DESKTOP-760JIBS>
2023-05-28 20:42:10 +0300

    Update

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,8 +1,6 @@
 import gameLogics from '../index.js';
 import { getRandomNumber } from '../random-number.js';
-
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
-
 const isNumberPrime = (number) => {
   for (let i = 2; i < number; i += 1) {
     if (number % i === 0 || number === 1) {
@@ -11,13 +9,11 @@ const isNumberPrime = (number) => {
   }
   return true;
 };
-
 const getAnswerAndQuestion = () => {
   const questionInGame = getRandomNumber();
   const rightAnswer = isNumberPrime(questionInGame) ? 'yes' : 'no';
   return [questionInGame, rightAnswer];
 };
-
 export default function prime() {
   gameLogics(description, getAnswerAndQuestion);
 }
